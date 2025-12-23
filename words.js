14ce6d5a3c0bd15af50ab4af58db8a8c2b7fe6fa
Davitayvaz <156727275+Davitayvaz@users.noreply.github.com>
2025-12-23 11:02:10 -0500

    Update words.js

--- words.js
+++ words.js
@@ -1,25 +1,119 @@
 const words = [
-    // Dec 15–23 filler (change anytime)
-    "HELLO",
-    "DAVIT",
-    "PEACE",
-    "JOY",
-    "WARM",
-    "LIGHT",
-    "KIND",
-    "HAPPY",
-    "TOMORROW",
-  
-    // Dec 24 →
-    "SANTA",
-    "PRESENT",
-    "TREE",
-    "SKIPPY",
-    "JAN",
-    "HUGS",
-    "SNOW",
-    "END",
-    "BEGINNING",
-    "POLARBEAR"
-  ];
-  
+  // Dec 15, 2025
+  "HELLO",
+
+  // Dec 16, 2025
+  "DAVIT",
+
+  // Dec 17, 2025
+  "PEACE",
+
+  // Dec 18, 2025
+  "JOY",
+
+  // Dec 19, 2025
+  "WARM",
+
+  // Dec 20, 2025
+  "LIGHT",
+
+  // Dec 21, 2025
+  "KIND",
+
+  // Dec 22, 2025
+  "HAPPY",
+
+  // Dec 23, 2025
+  "TOMORROW",
+
+  // Dec 24, 2025 🎄
+  "SANTA",
+
+  // Dec 25, 2025 🎁
+  "PRESENTS",
+
+  // Dec 26, 2025
+  "TREE",
+
+  // Dec 27, 2025
+  "SKIPPY",
+
+  // Dec 28, 2025
+  "JAN",
+
+  // Dec 29, 2025
+  "HUGS",
+
+  // Dec 30, 2025
+  "SNOW",
+
+  // Dec 31, 2025
+  "END",
+
+  // Jan 1, 2026 🎉
+  "BEGINNING",
+
+  // Jan 2, 2026
+  "POLARBEAR",
+
+
+  // Jan 3, 2026
+  "",
+
+  // Jan 4, 2026
+  "",
+
+  // Jan 5, 2026
+  "",
+
+  // Jan 6, 2026
+  "",
+
+  // Jan 7, 2026
+  "",
+
+  // Jan 8, 2026
+  "",
+
+  // Jan 9, 2026
+  "",
+
+  // Jan 10, 2026
+  "",
+
+  // Jan 11, 2026
+  "",
+
+  // Jan 12, 2026
+  "",
+
+  // Jan 13, 2026
+  "",
+
+  // Jan 14, 2026
+  "",
+
+  // Jan 15, 2026
+  "",
+
+  // Jan 16, 2026
+  "",
+
+  // Jan 17, 2026
+  "",
+
+  // Jan 18, 2026
+  "",
+
+  // Jan 19, 2026
+  "",
+
+  // Jan 20, 2026
+  "",
+
+  // Jan 21, 2026
+  "",
+
+  // Jan 22, 2026
+  ""
+];
